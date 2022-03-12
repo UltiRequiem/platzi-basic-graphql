c7a0bd894e27a70c278d656714cb03a902c05033
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-12 17:03:11 -0500

    :wrench: 😭😭😭😭😭
    When top level await + esm on node.js? i miss deno

--- src/lib/graphql_resolvers.ts
+++ src/lib/graphql_resolvers.ts
@@ -4,13 +4,14 @@ import type { IResolvers } from "@graphql-tools/utils";
 
 export const resolvers: IResolvers = {
   Query: {
-    courses: Database.courses,
+    courses() {
+      return Database.courses();
+    },
 
     course(_root, args: { id: string }) {
       return Database.course(args.id);
     },
   },
-
   Mutation: {
     createCourse(_root, { input }) {
       return Database.addCourse(input);
